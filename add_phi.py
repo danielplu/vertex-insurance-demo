4efe81eee080250b246eec10d66c15b33fd01720
Daniel Lu <dplu@google.com>
2022-01-04 14:00:01 -0500

    remove comment

--- add_phi.py
+++ add_phi.py
@@ -2,16 +2,7 @@ import csv
 from faker import Faker
 
 fake = Faker()
-'''
-v = open('/Users/dplu/Downloads/insurance.csv')
-r = csv.reader(v)
-row0 = next(r)
-row0.append('Phone Number')
-print(row0)
-for item in r:
-    item.append(fake.ssn())
-    print(item)
-'''
+
 
 with open('/Users/dplu/Downloads/insurance.csv','r') as csvinput:
     with open('/Users/dplu/Downloads/insurance_pii.csv', 'w') as csvoutput:
